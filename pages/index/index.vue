ce41e36485ad01074f5864d863b9400ede2dfbb8
liuxiaokun <liuxiaokun@baiyixinkeji.com>
2019-12-04 11:11:47 +0800

    优化轮播图

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,15 +1,22 @@
 <template>
 	<view>
-		<swiper class="swi" :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" indicator-active-color="#2d8cf0">
-			<swiper-item><image src="../../static/swiper/1.png" class="img"></image></swiper-item>
-			<swiper-item><image src="../../static/swiper/2.png" class="img"></image></swiper-item>
-			<swiper-item><image src="../../static/swiper/3.png" class="img"></image></swiper-item>
-		</swiper>
+		<uni-swiper-dot :info="info" :current="current" field="content" :mode="mode">
+			<swiper class="swi" :autoplay="true" :interval="3000" :duration="1000" @change="change">
+				<swiper-item><image src="../../static/swiper/1.png" class="img"></image></swiper-item>
+				<swiper-item><image src="../../static/swiper/2.png" class="img"></image></swiper-item>
+				<swiper-item><image src="../../static/swiper/3.png" class="img"></image></swiper-item>
+			</swiper>
+		</uni-swiper-dot>
 
 		<scroll-view scroll-y="true">
 			<block v-for="(item, index) in projectData" :key="index">
-				<uni-card :title="item.name" thumbnail="../../static/project.png" :extra="item.remark" 
-				:note="item.codeGitUrl" is-shadow="true" @tap="goFunctionPage(item.id, item.name)">
+				<uni-card
+					:title="item.name"
+					thumbnail="../../static/project.png"
+					:extra="item.remark"
+					:note="item.codeGitUrl"
+					is-shadow="true"
+					@tap="goFunctionPage(item.id, item.name)">
 					{{ item.desc }}
 				</uni-card>
 			</block>
@@ -18,44 +25,61 @@
 </template>
 
 <script>
-import {uniCard} from '@dcloudio/uni-ui';
+import { uniCard, uniSwiperDot } from '@dcloudio/uni-ui';
 
 export default {
-	components:{
-		uniCard
+	components: {
+		uniCard,
+		uniSwiperDot
 	},
 	data() {
 		return {
-			projectData:[]
+			info: [
+				{
+					content: '一叶知秋'
+				},
+				{
+					content: '吱吱呀呀'
+				},
+				{
+					content: '一览众山小'
+				}
+			],
+			current: 0,
+			mode: 'nav',
+			projectData: []
 		};
 	},
 	onLoad() {
-		this.loadProject()
+		this.loadProject();
 	},
 
-    onPullDownRefresh() {
-		this.loadProject()
+	onPullDownRefresh() {
+		this.loadProject();
 		console.log('load finish');
 		uni.stopPullDownRefresh();
-    },
+	},
 	methods: {
+		change(e) {
+			this.current = e.detail.current;
+		},
 		loadProject() {
-			console.log("start load project")
+			console.log('start load project');
 			uni.request({
 				method: 'GET',
 				url: 'http://192.168.2.246:3333/project',
 				dataType: 'JSON',
-				success: (res) => {
-					let dataObj = JSON.parse(res.data)
+				success: res => {
+					let dataObj = JSON.parse(res.data);
 					this.projectData = dataObj.data;
 				}
-			})
+			});
 		},
-		
+
 		goFunctionPage(projectId, title) {
-			console.log("click")
+			console.log('click');
 			uni.navigateTo({
-			    url: '/pages/function/function?id=' + projectId + '&title=' + title,
+				url: '/pages/function/function?id=' + projectId + '&title=' + title
 			});
 		}
 	}
