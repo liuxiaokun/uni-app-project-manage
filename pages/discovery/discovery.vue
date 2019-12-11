9424247e02c4767c38b72191c32c9b596ecc44f2
liuxiaokun <liuxiaokun@baiyixinkeji.com>
2019-12-11 11:36:00 +0800

    add 我的位置

--- pages/discovery/discovery.vue
+++ pages/discovery/discovery.vue
@@ -16,12 +16,34 @@ export default {
 					title:'百益信环保科技有限公司',
 					latitude: 37.56382,
 					longitude: 121.18263,
-					iconPath: '/static/postion_marker.png'
+					iconPath: '/static/location/postion_marker.png'
 				}
 			]
 		};
 	},
-	methods: {}
+	
+	onLoad() {
+		this.loadCurrentLocation();
+	},
+	
+	methods: {
+		loadCurrentLocation() {
+			uni.getLocation({
+			    type: 'wgs84',
+			    success: (res) => {
+			        console.log('当前位置的经度：' + res.longitude);
+			        console.log('当前位置的纬度：' + res.latitude);
+					
+					this.covers.push({
+						title:'我的位置',
+						latitude: res.latitude,
+						longitude: res.longitude,
+						iconPath: '/static/location/my_position.png'
+					})
+			    }
+			});
+		}
+	}
 };
 </script>
 
