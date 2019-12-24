d8f53cf792d6152eecffd9ee6efbc311152438e4
liuxiaokun <liuxiaokun@baiyixinkeji.com>
2019-12-24 16:23:15 +0800

    commit

--- pages/search/search.vue
+++ pages/search/search.vue
@@ -1,37 +1,35 @@
 <template>
-	<view>
-		<zy-search :is-focus="false" :theme="themeClass" :show-want="true" :speechEngine="speechEngine"
-		:hot-list="hotList" @getSearchText="getSearchText"></zy-search>
-	</view>
+    <view><zy-search :is-focus="false" :theme="themeClass" :show-want="true" :speechEngine="speechEngine" :hot-list="hotList" @getSearchText="getSearchText"></zy-search></view>
 </template>
 
 <script>
-	import zySearch from '@/components/zy-search/zy-search.vue'
-	export default {
-		components: {
-			zySearch
-		},
-        
-        onLoad(option) {
-            console.log(option.value)
-        },
-		data() {
-			return {
-				themeClass: 'block',
-				speechEngine: 'baidu', //语音识别引擎
-				hotList: ['栏目1','栏目2','栏目3','栏目4']	//初始化推荐列表
-			}
-		},
-		methods: {
-			getSearchText(e) {
-				uni.showToast({
-					title:'回调的搜索信息: ' + e,
-					icon:"none"
-				})
-			}
-		},
-	}
+import zySearch from '@/components/zy-search/zy-search.vue';
+export default {
+    components: {
+        zySearch
+    },
+
+    onLoad(option) {
+        console.log(option.value);
+    },
+    data() {
+        return {
+            themeClass: 'block',
+            //语音识别引擎
+            speechEngine: 'baidu',
+            //初始化推荐列表
+            hotList: ['百益新生活', '百益云智管', '百益项目管制']
+        };
+    },
+    methods: {
+        getSearchText(e) {
+            uni.showToast({
+                title: '回调的搜索信息: ' + e,
+                icon: 'none'
+            });
+        }
+    }
+};
 </script>
 
-<style>
-</style>
+<style></style>
